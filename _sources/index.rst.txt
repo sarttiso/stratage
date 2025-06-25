33507809afb1b4ebae45be8e4db11e6f8f62b5ef
sarttiso <sarttiso@users.noreply.github.com>
2025-06-25 17:28:17 +0000

    deploy: c5f3bc7ab1b1bd2ed4fdc59e9bba01dd6f62ad72

---
+++ _sources/index.rst.txt
@@ -0,0 +1,16 @@
+.. ========
+.. StratAge
+.. ========
+
+.. mdinclude:: ../readme.md
+
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   Toy Example <toy-example>
+   Module Reference <api/modules>
+   License <license>
+   Changelog <changelog>
---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.1.0.post1.dev1+gc5f3bc7',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
