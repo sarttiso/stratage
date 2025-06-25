02c10f3c78633c4f33e3a42003b7cd75b8c62896
Adrian Tasistro-Hart <adrian@tasistro-hart.com>
2025-06-25 09:03:14 -0700

    furo theme, no imported member documentation

--- docs/conf.py
+++ docs/conf.py
@@ -89,8 +89,7 @@ myst_enable_extensions = [
 # autodoc config
 autodoc_default_options = {
     'members': True,
-    'undoc-members': True,
-    'imported-members': True,
+    'undoc-members': True
 }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -175,7 +174,7 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
